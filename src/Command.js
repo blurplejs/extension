adc045d4969840d26e0c276e14c0b3d2ca06c00d
Christopher Mühl <christopher@dieschittigs.de>
2018-07-26 23:12:23 +0200

    Add some more command parsing options

--- src/Command.js
+++ src/Command.js
@@ -2,13 +2,47 @@ module.exports = class Command {
 
     constructor (extension, signature) {
         this.extension = extension
-        this.signature = signature
+        this.signature = this.parse(signature)
+    }
+
+    parse (signature) {
+        let argumentRegex = /{(.+?)}/g
+        let matches, args = []
+        let start = Infinity
+
+        while (matches = argumentRegex.exec(signature)) {
+            start = Math.min(matches.index)
+            args.push(match[1])
+        }
+
+        return {
+            command: signature.substring(0, start),
+            arguments: args
+        }
+    }
+
+    fillArguments (text) {
+        let list = {}
+
+        let input = text.replace(this.signature.command, '').split(' ')
+        for (let argument of this.signature.arguments) {
+            let match = []
+            if (match = argument.match(/(.+?)\?/)) {
+                // optional argument
+            } else if (match = argument.match(/(.+?)=(.+)/)) {
+                // default value
+            } else if (match = argument.match(/(.+?)\*/)) {
+                // variadic argument
+            }
+        }
+
+        return list
     }
 
     matches (message) {
         let content = message.content
 
-        return this.signature == content
+        return this.signature.command == content
     }
 
     call (message) {
@@ -16,7 +50,8 @@ module.exports = class Command {
             return
         }
 
-        this.handler.call(this.extension, message, {})
+        let args = this.fillArguments(message.content)
+        this.handler.call(this.extension, message, args)
     }
 
     then (handler) {
