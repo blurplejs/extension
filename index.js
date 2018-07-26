27a7a61d8eecdfeb302bd6131996af7b3e2dbafa
Christopher Mühl <christopher@dieschittigs.de>
2018-07-26 21:41:54 +0200

    Add a basic command implementation

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 module.exports = {
     Command: require('./src/Command'),
-    Bot: require('./src/Bot'),
-    BotCustomizer: require('./src/BotCustomizer')
+    Bot: require('./src/Bot')
 }
 
--- src/Command.js
+++ src/Command.js
@@ -1,16 +1,32 @@
 module.exports = class Command {
 
-    constructor (signature) {
+    constructor (extension, signature) {
+        this.extension = extension
         this.signature = signature
     }
 
-    then () {
+    matches (message) {
+        let content = message.content
+
+        return this.signature == content
+    }
+
+    call (message) {
+        if (!this.matches(message)) {
+            return
+        }
+
+        this.handler.call(this.extension, message, {})
+    }
+
+    then (handler) {
+        this.handler = handler
+
         return this
     }
 
-    synonym () {
+    synonym (name) {
         return this
     }
 
 }
-
